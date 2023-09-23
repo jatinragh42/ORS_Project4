6fc05405eb71d23847e6c5604ab4a6e36e251d33
jatinragh42 <jaydeepraghuwanshi97@gmail.com>
2023-09-23 11:56:32 +0530

    ok

---
+++ ORS_Project4/src/main/java/com/rays/pro4/Exception/RecordNotFoundException.java
@@ -0,0 +1,12 @@
+package com.rays.pro4.Exception;
+/**
+ * RecordNotFoundException thrown when a record not found occurred.
+ * 
+ * @author JATIN RAGHUWANSHI
+ * 
+ */
+public class RecordNotFoundException extends Exception {
+	public RecordNotFoundException(String msg){
+		super(msg);
+	}
+}
